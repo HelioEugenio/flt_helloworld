1bdc257315bb087c75935fd4e345253891002718
HelioEugenio <helio@helioeugenio.com.br>
2022-02-05 21:26:59 -0300

    quarto envio

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flt_helloworld/hello_world.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flt_helloworld/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
